0be7e2eab13d54a1de8603b34123e378f37fb389
SinaAkhavi <My30naAkhavi@gmail.com>
2023-02-11 17:33:22 +0330

    used element-ui in forms, table, button

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,24 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
+// import Vue from 'vue';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// import App from './App.vue';
 
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-});
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// });
 
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
 
 
 
